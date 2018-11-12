3432aafefa7b7dbf196aaef0a4a77e8dac30cdf5
gbourgeo <gillesbourgeoi@gmail.com>
2018-11-12 09:46:46 +0100

    removed capslock and numlock to a generic lockers table

--- srcs/handlers.c
+++ srcs/handlers.c
@@ -1,23 +1,9 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   handlers.c                                         :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: root </var/mail/root>                      +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2018/09/28 05:57:11 by root              #+#    #+#             */
-/*   Updated: 2018/10/31 18:37:48 by root             ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 #include "keylogger.h"
 
-int             shift_handler(int value, int state, int *modifier, int *capslock)
+int             shift_handler(int value, int state, int *modifier)
 {
 	if (state == 2)
 		return 0;
-	if (*capslock && (value == KEY_LEFTSHIFT || value == KEY_RIGHTSHIFT))
-		state = !state;
 	if (state == 0)
 		*modifier -= (1 << value);
 	else if (state == 1)
@@ -25,15 +11,14 @@ int             shift_handler(int value, int state, int *modifier, int *capslock
 	return 0;
 }
 
-int			lock_handler(int value, int state, int *modifier, int *capslock)
+int				lock_handler(int value, int state, int *modifier, int *locker)
 {
 	if (state != 1)
 		return 0;
-	if (value == 6)
-		*capslock = !(*capslock);
-	if (*capslock == 0)
-		*modifier -= (1 << KG_SHIFT);
+	locker[value] = !(locker[value]);
+	if (locker[value] == 0)
+		*modifier -= (1 << value);
 	else
-		*modifier += (1 << KG_SHIFT);
+		*modifier += (1 << value);
 	return 0;
 }
